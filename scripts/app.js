5f0cc59ad72a531687b03cd75f58b442f6b49c2d
omis-logistic <myorder.onlineshopping@gmail.com>
2025-11-20 13:14:25 +0800

    app.js
    update gas url

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 //scripts/app.js
 // ================= CONFIGURATION =================
 const CONFIG = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzVeeaV2vlRpU1KogITdiaILj0IJ41GoUqTn5d4-7uch4sEMbFh2A61ux9TF8N0QEd4/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwKMUh3JSNvDiB7V3E9ABIsL874mlaaE4Z1wTkec-8OZD5Jn-Q_S6ABrQ09Ae8lIDk_/exec',
   PROXY_URL: 'https://script.google.com/macros/s/AKfycbxIAX4irDMGRGzrQcnuQIP3-gAdMJ_tdAP9UDHr14s4deFBLu_-RjBRjZA8FgX3mrqtEQ/exec',
   SESSION_TIMEOUT: 3600,
   MAX_FILE_SIZE: 5 * 1024 * 1024,
